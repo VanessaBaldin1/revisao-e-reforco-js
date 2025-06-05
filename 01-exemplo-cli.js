b87b4db4acb33037041ccde82dc7e4f950d52561
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-05 11:47:19 -0300

    CLI escopo de variáveis e constantes JS

---
+++ 01-exemplo-cli.js
@@ -0,0 +1,16 @@
+// Habilitando o modo estrito da linguagem
+'use strict';
+
+let preco = 100;
+let resultado;
+
+const aluno = "fulano";
+console.log(aluno);
+
+if (preco >= 50){
+   resultado = "Beleza...";
+} else {
+   resultado = "Que droga...";
+}
+
+console.log(resultado);
