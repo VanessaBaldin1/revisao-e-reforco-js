9c2c3b10d3fa9becf3685fedb3b49e32607b124f
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-06 09:17:41 -0300

    função anônima não pode ser chamada antes da função

--- 03-funcoes.js
+++ 03-funcoes.js
@@ -8,3 +8,18 @@ function exemplo1(){
    console.log("Função nomeada");
 }
 
+console.log("************************************************");
+
+// exemplo2(); chamada aqui dá erro, pois não pode ser feita antes da declaração
+
+// Sintaxe tradicional , função anônima
+// Nesta sintaxe, a ordem IMPORTA
+
+const exemplo2 = function(){
+    console.log("Função anônima");
+    
+};
+
+// no caso da Anônima a chamada sempre é depois da declaração da função
+
+exemplo2(); //chamada (sempre após declarar a função anônima)
