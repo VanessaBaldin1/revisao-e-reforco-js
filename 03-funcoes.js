e42973bb4eb708bd78190e4fc2e6cb1e3c714643
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-06 09:47:08 -0300

    exercicio funções JS

--- 03-funcoes.js
+++ 03-funcoes.js
@@ -8,7 +8,12 @@ function exemplo1(){
    console.log("Função nomeada");
 }
 
-console.log("************************************************");
+function separador() {
+  console.log("\n************************************************\n");
+}
+
+separador();
+
 
 // exemplo2(); chamada aqui dá erro, pois não pode ser feita antes da declaração
 
@@ -23,3 +28,23 @@ const exemplo2 = function(){
 // no caso da Anônima a chamada sempre é depois da declaração da função
 
 exemplo2(); //chamada (sempre após declarar a função anônima)
+
+separador();
+
+// 1) Crie uma função (qualquer sintaxe) que receba dois valores numéricos como parâmetros, calcule a diferença entre estes valores e retorne o resultado calculado
+
+function menos(a, b) {
+   return a - b;  
+}
+
+//2) Chame essa função pelo menos duas vezes passando valores diferentes e guardando os resultados em variáveis de escopo global
+
+let resultado1 = menos(40, 20);
+
+let resultado2 = menos(50, 20);
+
+
+// 3) Apresente os resultados no console.
+
+let mensagem3 = `Os Resultados foram: ${resultado1} e ${resultado2}`;
+console.log(mensagem3);
