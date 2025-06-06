225a978a9e9bbd3fb982289bc5245fc75d260068
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-06 08:50:15 -0300

    Usa operador += para concacetar

--- 02-lidando-com-strings.js
+++ 02-lidando-com-strings.js
@@ -10,7 +10,16 @@ let estado = 'SP';
 
 // CONCATENAÇÃO tradicional usa operador 
 
-let mensagem1 = "meu nome é "+nome+" "+sobrenome+", tenho "+idade+" anos e moro em "+cidade+"/"+estado+".";
+// let mensagem1 = "meu nome é "+nome+" "+sobrenome+", tenho "+idade+" anos e moro em "+cidade+"/"+estado+".";
+
+// Usando operador += para concatenar "por pedaços/partes"
+
+let mensagem1 = "Meu nome é "+nome;
+mensagem1 += " "+sobrenome+", tenho " +idade;
+mensagem1 += " anos e moro em "+cidade;
+mensagem1 += "/"+estado+".";
+
+
 console.log(mensagem1);
 
 
