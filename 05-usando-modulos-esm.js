47297bb210e8238d1432a55c87e424f771911665
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-09 11:41:22 -0300

    resolução do exercicio

--- 05-usando-modulos-esm.js
+++ 05-usando-modulos-esm.js
@@ -39,5 +39,34 @@ console.log(pessoa.nome);
 console.log(livro);
 console.log(marias); // lembre-se: personagens é um apelido para alunos
 
+separador();
+
+// Exercícios
+//1) Crie um arquivo chamdo "funcoes.js" dentro da pasta "modulos-esm"
+//2) Dentro dele, programe 3 funções (USANDO ARROW FUNCTION):
+     //- Converter caracteres para MAIÚSCULO
+     //- Converter caracteres para MINÚSCULO
+     //- Formatar valor em moeda brasileira (real)
+
+//3) Exporte-as como módulos
+//4) No arquivo 05, importe e utilize estas funções passando textos e valores para testes.
+
+separador();
+
+//4) importando as funções do arquivo 'funcoes.js'
+
+import { converterLetraMaiuscula, converterLetraMinusculo, formatarvalorMoeda } from './modulos-esm/funcoes.js';
+
+// Acessando recursos do módulo 'funcoes.js'
+console.log(converterLetraMaiuscula("amazonas"));
+console.log(converterLetraMinusculo("ESTADOS UNIDOS"));
+console.log(formatarvalorMoeda(58996.89));
+
+
+
+
+
+
+
 
 
