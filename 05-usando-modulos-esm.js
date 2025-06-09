df83b7e6ff5d2134d996b830078dacd289e38f93
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-09 09:22:23 -0300

    cria arquivo 05 e cria package.json

---
+++ 05-usando-modulos-esm.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Importação de múltiplos recursos/módulos usando ESM
+// como é um objeto só chamar direto 
+import { saudacao, clientes } from './modulos-esm/exemplos.js';
+
+saudacao("Fulano de Tal");
+console.log(clientes);
