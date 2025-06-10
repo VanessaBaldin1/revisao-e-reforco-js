3fc32b396c78492e625592bd02eab638fb08c159
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-10 11:01:04 -0300

    cria arquivo 07- operador Rest ...

--- 07-operador-rest.js
+++ 07-operador-rest.js
@@ -1,2 +1,26 @@
 'use strict';
 
+// Sobre o operador rest ...
+//Usamos o rest para mesclar uma relação de parâmetros/argumentos para uma função
+//Na prática, os parâmetros se comportam como um array podendo receber iterações via loop.
+
+function somar(...valores) {
+   //Variável acumuladora
+   let total = 0;
+
+   //Iteração para somar e aguardar o total
+   for(const valor of valores){
+       total += valor;
+   }
+
+   return total;
+   
+}
+
+let resultado1 = somar(10, 20);
+let resultado2 = somar(20, 60, 80, 90);
+
+console.log(resultado1);
+console.log(resultado2);
+
+
