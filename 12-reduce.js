aa48aeba64e33b09621066a022c936bd224a3d84
Vanessa Baldin <vanbaldin08@gmail.com>
2025-06-16 09:09:37 -0300

    cria arquivo 12 reduce

---
+++ 12-reduce.js
@@ -0,0 +1,19 @@
+"use strict";
+
+import cursos from "./modulos-esm/cursos.js";
+import { separador } from "./modulos-esm/exemplos.js";
+
+// reduce (reduzir, redução, chegar a um resultado único)
+// Passa por elementos de um array e reduz a um único valor, que pode ser uma soma, média, concatenação, etc
+
+//A função callBack recebe dois parâmetros: o acumulador (valor que é acumulado até o momento), e o valor atual (elemento do array sendo processado)
+// primeiro parametro do reduce =>  e o segundo parametro é o contador (inicia com 0)
+// dentro do function (acumulador que inicia e seguida os valores)
+
+// Exemplo 1:
+const valores = [10, 5, 50, 200, 1000];
+const total = valores.reduce((acumulador, valor) => {
+  return acumulador + valor;
+}, 0);
+
+console.log(total);
